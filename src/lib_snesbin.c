46f730e64f2615c752b4bf78004b993fca129c9d
birch <bbbbbr@users.noreply.github.com>
2018-05-01 16:14:17 -0700

    Initial checkin of snes lib files

---
+++ src/lib_snesbin.c
@@ -0,0 +1,28 @@
+/*=======================================================================
+              SNES bin load / save plugin for the GIMP
+                 Copyright 2018 - X
+
+                 Useful : https://www.rpi.edu/dept/acm/packages/gimp/gimp-1.2.3/plug-ins/common/pcx.c
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=======================================================================*/
+
+#include "lib_snesbin.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <libgimp/gimp.h>
+
